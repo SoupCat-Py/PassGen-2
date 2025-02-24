8dbea99e0671a8de7003df7eeaa68b080982f8ea
Soup Cat <soupcat.py@gmail.com>
2025-02-24 12:32:32 -0700

    Added notes for soup
    stuff on how to implement sound effects later. I got some ideas

--- passgen_2.py
+++ passgen_2.py
@@ -33,6 +33,14 @@ text_dict = {'red'   : 'white',
              'purple': 'white',
              'pink'  : 'white'}
 
+# ADD DICT FOR SOUND EFFECT FILES
+# click, tab, save, error, etc
+
+# PLAYSOUND FUNCTION
+# in every element command - unless there's another way
+# one param: sound
+# if sounds are toggled: look at param and get that wav/mp3 file from dict
+
 lower       = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
 upper       = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']
 numbers     = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
